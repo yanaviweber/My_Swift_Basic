d6607304fc56e6a8544c0cab89e72f9663561eab
Vi Jane <creative.mashine@gmail.com>
2021-03-20 02:54:51 +0200

    Lesson 2: - Константы и переменные. Lesson 3: - Типы данных

---
+++ MyPlayground_L2.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+//Lesson ----- Константы и переменные -----/
+
+var str = "Hello, playground"
+var str1 = "Hello, Steve"
+
+str = "Hello, Xcode"
+
+str1 = "Hello, Tim"
---
+++ MyPlayground_L3.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+//Lesson ----- Типы данных -----/
+
+var str = "Hello, playground"
+
+// Character, String, Int, Double, Float, Bool
+
+let x: Character = "q"
+
+let y: String = "Russian Federation"
+
+let z: Double = 1.00005
+let z1: Double = 1.000_050_000_123_123_6
+
+let w: Float = 1.000006
+
+let d: Int = Int(123.123)
+let d1: Int = 123
+
+let b: Bool = true //false
+
+//преобразование типов с одного типа в другой
+
+let r = "false"
+let f = 123.123 //Double
+let v = Float(123.12312312312345356789)
+
