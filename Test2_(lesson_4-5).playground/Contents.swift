f9b1a58d05c41cefd25640b9d85efa8bc3746947
Vi Jane <creative.mashine@gmail.com>
2021-03-21 02:09:42 +0200

    Test 2: - Lesson 4, 5

---
+++ Test2_(lesson_4-5).playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+// ---- Test 2  --- Lessons 4, 5 ------------------
+
+var str = "Hello, playground"
+
+let i = "4"
+let j = i + i
+
+let number: Float = 9
+print("Число \(number) в квадрате \(number * number), и его квадратный корень \(sqrt(number))")
+
+// -----
+
+//нужно поменять let на var. Код не скомпилируется
+//let x: Int = 10
+
+var x: Int = 10
+
+x += 10
+print("We have got \(x) cakes")
